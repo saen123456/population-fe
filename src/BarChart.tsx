aa61c74163a23fd8c79af5a27b9ae155692e01e9
Pipat Angkaew <pipiat0909737525@gmail.com>
2024-08-15 21:31:35 +0700

    add: add useMemo for not rerender

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo } from "react";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -26,67 +26,75 @@ interface BarChartProps {
 }
 
 const BarChart: React.FC<BarChartProps> = ({ data, year }) => {
-  const populationCount = data[year]
-    ?.map((item: any) => +item.population)
-    .reduce((prev: number, curr: number) => prev + curr, 0)
-    .toLocaleString();
+  const populationCount = useMemo(() => {
+    return data[year]
+      ?.map((item: any) => +item.population)
+      .reduce((prev: number, curr: number) => prev + curr, 0)
+      .toLocaleString();
+  }, [data, year]);
 
-  const chartData = {
-    labels: data[year]?.map((item: any) => item.country),
-    datasets: [
-      {
-        label: `Population in ${year}`,
-        data: data[year]?.map((item: any) => item.population),
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+  const chartData = useMemo(
+    () => ({
+      labels: data[year]?.map((item: any) => item.country),
+      datasets: [
+        {
+          label: `Population in ${year}`,
+          data: data[year]?.map((item: any) => item.population),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    }),
+    [data, year]
+  );
 
-  const options: ChartOptions<"bar"> = {
-    indexAxis: "y", // This will make the bar chart horizontal
-    scales: {
-      x: {
-        beginAtZero: true,
-        title: {
-          display: true,
-          text: "Population",
+  const options: ChartOptions<"bar"> = useMemo(
+    () => ({
+      indexAxis: "y", // This will make the bar chart horizontal
+      scales: {
+        x: {
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: "Population",
+          },
+        },
+        y: {
+          title: {
+            display: true,
+            text: "Country",
+          },
         },
       },
-      y: {
-        title: {
-          display: true,
-          text: "Country",
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        tooltip: {
+          mode: "index",
+          intersect: false,
         },
       },
-    },
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      tooltip: {
-        mode: "index",
-        intersect: false,
-      },
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-  };
+      responsive: true,
+      maintainAspectRatio: false,
+    }),
+    []
+  );
 
   return (
     <div style={{ height: "500px", width: "800px" }}>
