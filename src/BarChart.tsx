eed895db16b1461b27a2434567e5f5b2db497f57
Pipat Angkaew <pipiat0909737525@gmail.com>
2024-08-15 19:43:29 +0700

    update bar chart

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -26,69 +26,78 @@ interface BarChartProps {
 }
 
 const BarChart: React.FC<BarChartProps> = ({ data, year }) => {
-    const chartData = {
-        labels: data[year].map((item: any) => item.country),
-        datasets: [
-            {
-                label: `Population in ${year}`,
-                data: data[year].map((item: any) => item.population),
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                    "rgba(255, 159, 64, 0.2)",
-                ],
-                borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(153, 102, 255, 1)",
-                    "rgba(255, 159, 64, 1)",
-                ],
-                borderWidth: 1,
-            },
+  const populationCount = data[year]
+    ?.map((item: any) => +item.population)
+    .reduce((prev: number, curr: number) => prev + curr, 0)
+    .toLocaleString();
+
+  const chartData = {
+    labels: data[year]?.map((item: any) => item.country),
+    datasets: [
+      {
+        label: `Population in ${year}`,
+        data: data[year]?.map((item: any) => item.population),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
         ],
-    };
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-    const options: ChartOptions<"bar"> = {
-        indexAxis: "y", // This will make the bar chart horizontal
-        scales: {
-            x: {
-                beginAtZero: true,
-                title: {
-                    display: true,
-                    text: "Population",
-                },
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: "Country",
-                },
-            },
+  const options: ChartOptions<"bar"> = {
+    indexAxis: "y", // This will make the bar chart horizontal
+    scales: {
+      x: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: "Population",
         },
-        plugins: {
-            legend: {
-                position: "top",
-            },
-            tooltip: {
-                mode: "index",
-                intersect: false,
-            },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Country",
         },
-        responsive: true,
-        maintainAspectRatio: false,
-    };
+      },
+    },
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      tooltip: {
+        mode: "index",
+        intersect: false,
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  };
 
-    return (
-        <div style={{ height: "500px", width: "800px" }}>
-            <Bar data={chartData} options={options} />
-        </div>
-    );
+  return (
+    <div style={{ height: "500px", width: "800px" }}>
+      <Bar data={chartData} options={options} />
+
+      <div style={{ textAlign: "end" }}>
+        <h1>{year}</h1>
+        <h2>Total: {populationCount}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default BarChart;
-
